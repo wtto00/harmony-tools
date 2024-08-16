a8f642f9f9f43def11f9694feffa8f08777f3136
wtto00 <wtto00@outlook.com>
2024-08-16 20:07:58 +0800

    fix libusb_share path

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,25 +1,40 @@
 fn main() {
-    #[cfg(target_os = "macos")]
     copy_libusb_share();
 
     tauri_build::build()
 }
 
-#[cfg(target_os = "macos")]
 fn copy_libusb_share() {
     use std::{fs, path::Path};
-
-    #[cfg(target_arch = "x86_64")]
-    {
-        let src = Path::new("resources/libusb_shared/libusb_shared.dylib-x86_64-apple-darwin");
-        let dest = Path::new("libusb_shared.dylib");
-        fs::copy(src, dest).unwrap();
-    }
-
-    #[cfg(target_arch = "aarch64")]
-    {
-        let src = Path::new("resources/libusb_shared/libusb_shared.dylib-aarch64-apple-darwin");
-        let dest = Path::new("libusb_shared.dylib");
-        fs::copy(src, dest).unwrap();
-    }
+    let (src, dest) = {
+        #[cfg(all(target_os = "macos", target_arch = "x86_64"))]
+        {
+            (
+                Path::new("resources/libusb_shared/libusb_shared.dylib-x86_64-apple-darwin"),
+                Path::new("libusb_shared.dylib"),
+            )
+        }
+        #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+        {
+            (
+                Path::new("resources/libusb_shared/libusb_shared.dylib-aarch64-apple-darwin"),
+                Path::new("libusb_shared.dylib"),
+            )
+        }
+        #[cfg(target_os = "windows")]
+        {
+            (
+                Path::new("resources/libusb_shared/libusb_shared.dll"),
+                Path::new("libusb_shared.dll"),
+            )
+        }
+        #[cfg(target_os = "linux")]
+        {
+            (
+                Path::new("resources/libusb_shared/libusb_shared.so"),
+                Path::new("libusb_shared.so"),
+            )
+        }
+    };
+    fs::copy(src, dest).unwrap();
 }
